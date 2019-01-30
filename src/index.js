076beb061f222c019a6433c4bf824b32a1966ba3
BulatTH <bulatxajrutdinoff@yandex.ru>
2019-01-30 06:06:40 +0300

    .

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/common';
+import './css/main.css';
+import './sass/main.sass';
+
+// import 'vue';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Vue from 'vue';
+window.Vue = require('vue');
